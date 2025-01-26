256abf55b7ff25b99d527996a8f1171fa429c641
Saiqul Haq <saiqulhaq@gmail.com>
2025-01-26 15:16:40 +0700

    test send email with interceptor

--- project/50115/pr_50115/config/initializers/mail_interceptors.rb
+++ project/50115/pr_50115/config/initializers/mail_interceptors.rb
@@ -1,5 +1,5 @@
-# Rails.application.configure do
-#   if Rails.env.development?
-#     config.action_mailer.interceptors = %w[SandboxEmailInterceptor]
-#   end
-# end
+Rails.application.configure do
+  if Rails.env.development?
+    config.action_mailer.interceptors = %w[SandboxEmailInterceptor]
+  end
+end
